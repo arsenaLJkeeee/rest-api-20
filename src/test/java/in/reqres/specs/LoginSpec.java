bf6c5c6d3f917b3196480af29aef8d31be45acdf
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-24 17:03:24 +0300

    remove unnesc imports

--- src/test/java/in/reqres/specs/LoginSpec.java
+++ src/test/java/in/reqres/specs/LoginSpec.java
@@ -1,7 +1,6 @@
 package in.reqres.specs;
 
 import in.reqres.helpers.CustomAllureListener;
-import in.reqres.models.UserData;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -10,8 +9,6 @@ import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.core.IsNull.notNullValue;
 
 public class LoginSpec {
 
