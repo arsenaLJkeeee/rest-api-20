aee5ad0b8c811ab70868a1d2a1ea13c371ae731e
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-24 19:00:03 +0300

    add steps

--- src/test/java/in/reqres/tests/LoginExtendedTests.java
+++ src/test/java/in/reqres/tests/LoginExtendedTests.java
@@ -4,6 +4,7 @@ import in.reqres.models.UserData;
 import in.reqres.specs.LoginSpec;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -13,6 +14,7 @@ public class LoginExtendedTests extends TestBase {
 
     @Test
     void successfulGetListOfUsers() {
+        step("Get list of users", () -> {
         given()
                 .spec(LoginSpec.getRequestSpecification())
                 .contentType(JSON)
@@ -21,6 +23,7 @@ public class LoginExtendedTests extends TestBase {
                 .then()
                 .spec(LoginSpec.getResponseSpecification())
                 .body(matchesJsonSchemaInClasspath("schemas/users-schema.json"));
+    });
     }
 
     @Test
@@ -29,16 +32,18 @@ public class LoginExtendedTests extends TestBase {
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("pistol");
 
-        given()
-                .spec(LoginSpec.getRequestSpecification())
-                .contentType(JSON)
-                .body(authData)
-                .when()
-                .post("/register")
-                .then()
-                .spec(LoginSpec.getResponseSpecification())
-                .body("token", notNullValue())
-                .body(matchesJsonSchemaInClasspath("schemas/get-token-after-registration-schema.json"));
+        step("Register and get token", () -> {
+            given()
+                    .spec(LoginSpec.getRequestSpecification())
+                    .contentType(JSON)
+                    .body(authData)
+                    .when()
+                    .post("/register")
+                    .then()
+                    .spec(LoginSpec.getResponseSpecification())
+                    .body("token", notNullValue())
+                    .body(matchesJsonSchemaInClasspath("schemas/get-token-after-registration-schema.json"));
+        });
     }
 
     @Test
@@ -47,35 +52,41 @@ public class LoginExtendedTests extends TestBase {
         authData.setName("morpheus");
         authData.setJob("leader");
 
-        given()
-                .spec(LoginSpec.getRequestSpecification()) // Используем общую спецификацию для запроса
-                .body(authData)
-                .when()
-                .post("/users")
-                .then()
-                .spec(LoginSpec.getResponseSpecificationFor201()) // Используем отдельную спецификацию для ответа с кодом 201
-                .body("id", notNullValue());
+        step("Create a new user", () -> {
+            given()
+                    .spec(LoginSpec.getRequestSpecification()) // Используем общую спецификацию для запроса
+                    .body(authData)
+                    .when()
+                    .post("/users")
+                    .then()
+                    .spec(LoginSpec.getResponseSpecificationFor201()) // Используем отдельную спецификацию для ответа с кодом 201
+                    .body("id", notNullValue());
+        });
     }
 
     @Test
     void negativeUserNotFoundTest() {
-        given()
-                .spec(LoginSpec.getRequestSpecification())
-                .contentType(JSON)
-                .when()
-                .get("/unknown/23")
-                .then()
-                .statusCode(404);
+        step("Check user not found", () -> {
+            given()
+                    .spec(LoginSpec.getRequestSpecification())
+                    .contentType(JSON)
+                    .when()
+                    .get("/unknown/23")
+                    .then()
+                    .statusCode(404);
+        });
     }
 
     @Test
     void successfulDeleteOfUser204() {
-        given()
-                .spec(LoginSpec.getRequestSpecification())
-                .contentType(JSON)
-                .when()
-                .delete("/users/2")
-                .then()
-                .statusCode(204);
+        step("Delete user", () -> {
+            given()
+                    .spec(LoginSpec.getRequestSpecification())
+                    .contentType(JSON)
+                    .when()
+                    .delete("/users/2")
+                    .then()
+                    .statusCode(204);
+        });
     }
 }
